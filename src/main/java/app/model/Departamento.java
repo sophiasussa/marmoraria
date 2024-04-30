ccbfb8db3b9001c793a9b4950a7cdc477c4ee083
sophiasussa <sophiasussa@outlook.com>
2024-04-30 09:02:32 -0300

    Cargo e Departamento Model

---
+++ src/main/java/app/model/Departamento.java
@@ -0,0 +1,22 @@
+package app.model;
+
+public class Departamento {
+    private int id;
+    private String nome;
+
+    public int getId(){
+        return id;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public void setNome(){
+        this.nome = nome;
+    }
+}
