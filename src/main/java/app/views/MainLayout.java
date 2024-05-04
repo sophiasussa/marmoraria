4907009da6af2011e83ad5e8473dbc545b8cfd7e
sophiasussa <sophiasussa@outlook.com>
2024-05-03 21:01:48 -0300

    Criado a parte de Navegação

--- src/main/java/app/views/MainLayout.java
+++ src/main/java/app/views/MainLayout.java
@@ -14,16 +14,24 @@ import app.views.tarefasgerais.TarefasGeraisView;
 import app.views.tarefasgeraisconcluido.TarefasGeraisConcluidoView;
 import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.applayout.DrawerToggle;
+import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.Footer;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.html.Header;
+import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.Scroller;
 import com.vaadin.flow.component.sidenav.SideNav;
 import com.vaadin.flow.component.sidenav.SideNavItem;
+import com.vaadin.flow.dom.Element;
 import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.RouterLink;
 import com.vaadin.flow.theme.lumo.LumoUtility;
 import org.vaadin.lineawesome.LineAwesomeIcon;
+import com.vaadin.flow.component.icon.Icon;
+
+
 
 /**
  * The main view is a top-level placeholder for other views.
@@ -49,39 +57,52 @@ public class MainLayout extends AppLayout {
     }
 
     private void addDrawerContent() {
-        H1 appName = new H1("My App");
+        H1 appName = new H1("Marmosoft");
         appName.addClassNames(LumoUtility.FontSize.LARGE, LumoUtility.Margin.NONE);
         Header header = new Header(appName);
 
-        Scroller scroller = new Scroller(createNavigation());
+        SideNav registro = createNavigation();
 
-        addToDrawer(header, scroller, createFooter());
+        SideNav nav = createNavegation2();
+
+        addToDrawer(header, registro, nav, createFooter());
     }
 
     private SideNav createNavigation() {
+        SideNav registro = new SideNav();
+
+        registro.setLabel("Registros");
+        registro.setCollapsible(true);
+        registro.setExpanded(false);
+        registro.addItem(new SideNavItem("Cliente", ClienteView.class, VaadinIcon.GROUP.create()));
+        registro.addItem(new SideNavItem("Cliente2", NovoClienteView.class, VaadinIcon.GROUP.create()));
+        registro.addItem(new SideNavItem("Fornecedor", FornecedorView.class, LineAwesomeIcon.ADDRESS_BOOK.create()));
+        registro.addItem(new SideNavItem("Novo Fornecedor", NovoFornecedorView.class, LineAwesomeIcon.ADDRESS_BOOK.create()));
+        registro.addItem(new SideNavItem("Funcionários", FuncionariosView.class, VaadinIcon.KEY.create()));
+        registro.addItem(new SideNavItem("Novo Funcionário", NovoFuncionarioView.class, VaadinIcon.KEY.create()));
+
+        registro.getElement().appendChild(new Element("hr"));
+
+        return registro;
+    }
+
+    private SideNav createNavegation2(){
         SideNav nav = new SideNav();
 
-        nav.addItem(new SideNavItem("Cliente", ClienteView.class, LineAwesomeIcon.PENCIL_RULER_SOLID.create()));
-        nav.addItem(new SideNavItem("NovoCliente", NovoClienteView.class, LineAwesomeIcon.PENCIL_RULER_SOLID.create()));
-        nav.addItem(new SideNavItem("Fornecedor", FornecedorView.class, LineAwesomeIcon.PENCIL_RULER_SOLID.create()));
-        nav.addItem(new SideNavItem("NovoFornecedor", NovoFornecedorView.class,
-                LineAwesomeIcon.PENCIL_RULER_SOLID.create()));
         nav.addItem(
-                new SideNavItem("Funcionarios", FuncionariosView.class, LineAwesomeIcon.PENCIL_RULER_SOLID.create()));
-        nav.addItem(new SideNavItem("NovoFuncionario", NovoFuncionarioView.class,
-                LineAwesomeIcon.PENCIL_RULER_SOLID.create()));
-        nav.addItem(
-                new SideNavItem("TarefasGerais", TarefasGeraisView.class, LineAwesomeIcon.PENCIL_RULER_SOLID.create()));
-        nav.addItem(
-                new SideNavItem("PerfilUsuario", PerfilUsuarioView.class, LineAwesomeIcon.PENCIL_RULER_SOLID.create()));
-        nav.addItem(new SideNavItem("NovoUsuario", NovoUsuarioView.class, LineAwesomeIcon.PENCIL_RULER_SOLID.create()));
-        nav.addItem(new SideNavItem("CriarPerfil", CriarPerfilView.class, LineAwesomeIcon.PENCIL_RULER_SOLID.create()));
-        nav.addItem(new SideNavItem("TarefasGeraisConcluido", TarefasGeraisConcluidoView.class,
-                LineAwesomeIcon.PENCIL_RULER_SOLID.create()));
-        nav.addItem(new SideNavItem("AddTarefasGerais", AddTarefasGeraisView.class,
-                LineAwesomeIcon.PENCIL_RULER_SOLID.create()));
+                new SideNavItem("TarefasGerais", TarefasGeraisView.class, VaadinIcon.NOTEBOOK.create()));
+        nav.addItem(new SideNavItem("NovoUsuario", NovoUsuarioView.class, LineAwesomeIcon.COG_SOLID.create()));
+        nav.addItem(new SideNavItem("CriarPerfil", CriarPerfilView.class, LineAwesomeIcon.COG_SOLID.create()));
+        nav.addItem(new SideNavItem("TarefasGeraisConcluido", TarefasGeraisConcluidoView.class, VaadinIcon.NOTEBOOK.create()));
+        nav.addItem(new SideNavItem("AddTarefasGerais", AddTarefasGeraisView.class, VaadinIcon.NOTEBOOK.create()));
+        
+        nav.getElement().appendChild(new Element("hr"));
 
+        nav.addItem(
+            new SideNavItem("PerfilUsuario", PerfilUsuarioView.class, LineAwesomeIcon.USER.create()));
+        
         return nav;
+
     }
 
     private Footer createFooter() {
